592f8b5b75b5aa15c35a41287117e5429c6284a9
Pranay Harjai <pranayharjai7@gmail.com>
2026-05-09 01:53:06 +0200

    chore: use import for SeedExercisesUseCase in MainActivity

--- app/src/main/java/com/trackme/MainActivity.kt
+++ app/src/main/java/com/trackme/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.lifecycleScope
 import com.trackme.ui.navigation.TrackMeNavGraph
 import com.trackme.ui.theme.TrackMeTheme
+import com.trackme.domain.usecase.SeedExercisesUseCase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,7 +15,7 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject lateinit var seedExercises: com.trackme.domain.usecase.SeedExercisesUseCase
+    @Inject lateinit var seedExercises: SeedExercisesUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
